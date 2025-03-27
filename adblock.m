337d7c5ea86c3c1243c7a3f80bf281cd448b32c3
stfosem <admin@gamesofts.net>
2025-03-27 00:51:27 -0700

    Update adblock.m

--- adblock.m
+++ adblock.m
@@ -727,31 +727,11 @@ void my_NSNetServiceResolve(id self, SEL _cmd) {
 }
 
 static void blocked_task_resume(id self, SEL __unused _cmd) {
-    NSError *error = [NSError errorWithDomain:NSURLErrorDomain 
+    if ([self respondsToSelector:@selector(error)]) {
+        NSError *error = [NSError errorWithDomain:NSURLErrorDomain 
                                            code:NSURLErrorCancelled 
                                        userInfo:nil];
-
-    if ([self respondsToSelector:@selector(error)]) {
         [self setValue:error forKey:@"error"];
-    }
-
-    id completionHandler = nil;
-    if ([self respondsToSelector:@selector(completionHandler)]) {
-        completionHandler = [self valueForKey:@"completionHandler"];
-    }
-    
-    if (completionHandler) {
-        void (^safeHandler)(NSData *, NSURLResponse *, NSError *) = ^(NSData *data, NSURLResponse *response, NSError *err) {
-            ((void (^)(NSData *, NSURLResponse *, NSError *))completionHandler)(data, response, err);
-        };
-        
-        if (strcmp(dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL), dispatch_queue_get_label(dispatch_get_main_queue())) == 0) {
-            safeHandler(nil, nil, error);
-        } else {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                safeHandler(nil, nil, error);
-            });
-        }
     }
 }
 
